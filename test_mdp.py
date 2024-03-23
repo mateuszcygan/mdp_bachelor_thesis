505178558cdc23e028354a5ae0b8b64448371bc3
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-03-23 18:56:54 +0100

    Tests taken over from previous repository

---
+++ test_mdp.py
@@ -0,0 +1,23 @@
+import unittest
+import mdp
+
+class TestMdpSets(unittest.TestCase):
+
+    def setUp(self):
+        self.states = mdp.generate_states()
+        self.actions = mdp.generate_actions()
+
+    def test_generate_states_non_empty(self):
+        self.assertTrue(self.states)  # Assert that states is not empty
+
+    def test_generate_states_type(self):
+        self.assertIsInstance(self.states, list)  # Assert that states is a list
+
+    def test_generate_actions_non_empty(self):
+        self.assertTrue(self.actions)  # Assert that actions is not empty
+
+    def test_generate_actions_type(self):
+        self.assertIsInstance(self.actions, list)  # Assert that actions is a list
+
+if __name__ == '__main__':
+    unittest.main()
