df2e9de0699d39d6c0c497102dce6a1274ffad0c
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-04-03 17:58:37 +0200

    Test for correct initial probabilities, two other tests commented out as new modular functions in implementation

--- test_algorithm.py
+++ test_algorithm.py
@@ -1,40 +1,81 @@
+import random
 import unittest
+
 import algorithm
 import mdp
 
-class TestLearnProbabilities(unittest.TestCase):
 
-    # Test if sum of approximated probabilities for each state is equals (close to) 1.0
-    
-    def test_prob_sum_finite(self):
+class TestEssentialFunctions(unittest.TestCase):
 
-        for _ in range(100): # Test for 100 different MDPs
+    def setUp(self):
+        self.states = ["s0", "s1"]
+        self.actions = ["a0", "a1"]
+        self.probabilities = {
+            "s0": {"a0": {"s0": 0.75, "s1": 0.25}, "a1": {"s0": 0.43, "s1": 0.57}},
+            "s1": {"a1": {"s0": 0.4, "s1": 0.6}},
+        }
+        self.rewards = {
+            "s0": {"a0": {"s0": 3, "s1": 4}, "a1": {"s0": 0, "s1": -2}},
+            "s1": {"a1": {"s0": 2, "s1": -3}},
+        }
 
-            mdp_object = mdp.createMDP()
-            iteration_number = 1000
+    def test_assign_initial_probabilities(self):
 
-            result = algorithm.learn_probabilities_finite(mdp_object, iteration_number)
+        expected_probabilities = {
+            "s0": {"a0": {"s0": 0.5, "s1": 0.5}, "a1": {"s0": 0.5, "s1": 0.5}},
+            "s1": {"a1": {"s0": 0.5, "s1": 0.5}},
+        }
 
-            # Check if the sum of all probabilities is close to 1
-            for state, action_foll_states in result.items():
-                for action, foll_states in action_foll_states.items():
-                    state_prob_sum = sum(list(foll_states.values()))
-                    self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
-    
-    def test_prob_sum_convergence(self):
+        initial_probabilities = algorithm.assign_initial_probabilities(
+            self.states, self.probabilities
+        )
 
-        for _ in range(100): # Test for 100 different MDPs
+        self.assertDictEqual(expected_probabilities, initial_probabilities)
 
-            mdp_object = mdp.createMDP()
-            threshold = 0.001
 
-            result = algorithm.learn_probabilities_convergence(mdp_object, threshold)
+# class TestLearnProbabilities(unittest.TestCase):
 
-            # Check if the sum of all probabilities is close to 1
-            for state, action_foll_states in result.items():
-                for action, foll_states in action_foll_states.items():
-                    state_prob_sum = sum(list(foll_states.values()))
-                    self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
+#     def setUp(self):
+#         self.states_num = random.randint(2, 9)
+#         self.actions_num = random.randint(2, 9)
+#         self.min_reward = random.randint(-8, 0)
+#         self.max_reward = random.randint(1, 8)
 
-if __name__ == '__main__':
+#     # Test if sum of approximated probabilities for each state is equals (close to) 1.0
+#     def test_prob_sum_finite(self):
+
+#         for _ in range(100):  # Test for 100 different MDPs
+
+#             mdp_object = mdp.createMDP(
+#                 self.states_num, self.actions_num, self.min_reward, self.max_reward
+#             )
+#             iteration_number = 1000
+
+#             result = algorithm.learn_probabilities_finite(mdp_object, iteration_number)
+
+#             # Check if the sum of all probabilities is close to 1
+#             for state, action_foll_states in result.items():
+#                 for action, foll_states in action_foll_states.items():
+#                     state_prob_sum = sum(list(foll_states.values()))
+#                     self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
+
+#     # def test_prob_sum_convergence(self):
+
+#     #     for _ in range(100):  # Test for 100 different MDPs
+
+#     #         mdp_object = mdp.createMDP(
+#     #             self.states_num, self.actions_num, self.min_reward, self.max_reward
+#     #         )
+#     #         threshold = 0.001
+
+#     #         result = algorithm.learn_probabilities_convergence(mdp_object, threshold)
+
+#     #         # Check if the sum of all probabilities is close to 1
+#     #         for state, action_foll_states in result.items():
+#     #             for action, foll_states in action_foll_states.items():
+#     #                 state_prob_sum = sum(list(foll_states.values()))
+#     #                 self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
+
+
+if __name__ == "__main__":
     unittest.main()
