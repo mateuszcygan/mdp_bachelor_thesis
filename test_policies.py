3f1752f79f37acf65ecfaf76debcfc05b6f71a22
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-04-03 16:51:01 +0200

    Test with equal rewards added

--- test_policies.py
+++ test_policies.py
@@ -54,5 +54,34 @@ class TestMdpSets(unittest.TestCase):
         self.assertDictEqual(policy_dead_end_finite, policy)
         self.assertDictEqual(V_dead_end_convergence, V_dead_end_finite)
 
+    # Test policy if all rewards have equal value
+    # crucial: 'max' function in Python - chooses FIRST biggest element from an array
+    def test_equal_rewards_values(self):
+        S = ['s0', 's1', 's2']
+        A = ['a0', 'a1', 'a2', 'a3']
+        R = {
+            's0': 
+                {'a0': {'s0': 4, 's1': 4, 's2': 4}, 'a1': {'s0': 4, 's1': 4, 's2': 4}, 'a2': {'s0': 4, 's1': 4, 's2': 4}, 'a3': {'s0': 4, 's1': 4, 's2': 4}},
+            's1': 
+                {'a0': {'s0': 4, 's1': 4, 's2': 4}, 'a1': {'s0': 4, 's1': 4, 's2': 4}, 'a2': {'s0': 4, 's1': 4, 's2': 4}, 'a3': {'s0': 4, 's1': 4, 's2': 4}}, 
+            's2': 
+                {'a0': {'s0': 4, 's1': 4, 's2': 4}, 'a1': {'s0': 4, 's1': 4, 's2': 4}, 'a2': {'s0': 4, 's1': 4, 's2': 4}, 'a3': {'s0': 4, 's1': 4, 's2': 4}}
+            }
+        P = {
+            's0': 
+                {'a0': {'s0': 0.17, 's1': 0.79, 's2': 0.04}, 'a1': {'s0': 0.71, 's1': 0.12, 's2': 0.17}, 'a2': {'s0': 0.89, 's1': 0.0, 's2': 0.11}, 'a3': {'s0': 0.38, 's1': 0.1, 's2': 0.52}},
+            's1': 
+                {'a0': {'s0': 0.7, 's1': 0.27, 's2': 0.03}, 'a1': {'s0': 0.58, 's1': 0.05, 's2': 0.37}, 'a2': {'s0': 0.82, 's1': 0.04, 's2': 0.14}, 'a3': {'s0': 0.79, 's1': 0.04, 's2': 0.17}},
+            's2': 
+                {'a0': {'s0': 0.02, 's1': 0.47, 's2': 0.51}, 'a1': {'s0': 0.78, 's1': 0.1, 's2': 0.12}, 'a2': {'s0': 0.15, 's1': 0.17, 's2': 0.68}, 'a3': {'s0': 0.79, 's1': 0.05, 's2': 0.16}}
+            }
+
+        mdp_equal_rewards = mdp.MDP(S, A, P, R)
+
+        V, policy = policies.value_iteration_finite(mdp_equal_rewards, 1, 1)
+        expected_policy = {'s0' : 'a0', 's1' : 'a0', 's2' : 'a0'}
+
+        self.assertDictEqual(policy, expected_policy)
+
 if __name__ == '__main__':
     unittest.main()
