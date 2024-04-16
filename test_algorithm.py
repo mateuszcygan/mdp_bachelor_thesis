a707407b4e4815678d55a3c0505ef70785810e0c
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-04-16 15:51:16 +0200

    Tests adjusted to new code divided into functions

--- test_algorithm.py
+++ test_algorithm.py
@@ -26,6 +26,7 @@ class TestEssentialFunctions(unittest.TestCase):
             "s0": {"a0": {"s0": 1.0, "s1": 0}, "a1": {"s0": 0, "s1": 1.0}},
             "s1": {"a1": {"s0": 0.4, "s1": 0.6}},
         }
+        self.states_hits = algorithm.create_states_hits_dictionary(self.probabilities)
 
     # Test if initial probabilities are initially correct assigned (equal probabilities dependent on states' number)
     def test_assign_initial_approx_probabilities(self):
@@ -46,8 +47,8 @@ class TestEssentialFunctions(unittest.TestCase):
 
         expected_next_state = "s1"
 
-        next_state = algorithm.execute_action(
-            self.states, self.probabilities_small, "s0", "a1"
+        next_state, states_hits = algorithm.execute_action(
+            self.states, self.probabilities_small, "s0", "a1", self.states_hits
         )
 
         self.assertEqual(expected_next_state, next_state)
@@ -102,48 +103,39 @@ class TestEssentialFunctions(unittest.TestCase):
         self.assertDictEqual(approximated_prob, expected_approx_prob)
 
 
-# class TestLearnProbabilities(unittest.TestCase):
+class TestLearnProbabilities(unittest.TestCase):
 
-#     def setUp(self):
-#         self.states_num = random.randint(2, 9)
-#         self.actions_num = random.randint(2, 9)
-#         self.min_reward = random.randint(-8, 0)
-#         self.max_reward = random.randint(1, 8)
+    def setUp(self):
+        self.states_num = random.randint(2, 9)
+        self.actions_num = random.randint(2, 9)
+        self.min_reward = random.randint(-8, 0)
+        self.max_reward = random.randint(1, 8)
 
-#     # Test if sum of approximated probabilities for each state is equals (close to) 1.0
-#     def test_prob_sum_finite(self):
+    # Test if sum of approximated probabilities for each state is equals (close to) 1.0
+    def test_prob_sum_finite(self):
 
-#         for _ in range(100):  # Test for 100 different MDPs
+        for _ in range(100):  # Test for 100 different MDPs
 
-#             mdp_object = mdp.createMDP(
-#                 self.states_num, self.actions_num, self.min_reward, self.max_reward
-#             )
-#             iteration_number = 1000
+            mdp_object = mdp.createMDP(
+                self.states_num, self.actions_num, self.min_reward, self.max_reward
+            )
 
-#             result = algorithm.learn_probabilities_finite(mdp_object, iteration_number)
+            S = mdp_object.states
+            P = mdp_object.probabilities
+            approx_prob = algorithm.assign_initial_approx_probabilities(S, P)
+            states_hits = algorithm.create_states_hits_dictionary(P)
+            current_state = "s0"
+            iteration_number = 100
 
-#             # Check if the sum of all probabilities is close to 1
-#             for state, action_foll_states in result.items():
-#                 for action, foll_states in action_foll_states.items():
-#                     state_prob_sum = sum(list(foll_states.values()))
-#                     self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
+            result, current_state = algorithm.systematic_learning(
+                S, P, approx_prob, states_hits, current_state, iteration_number
+            )
 
-#     # def test_prob_sum_convergence(self):
-
-#     #     for _ in range(100):  # Test for 100 different MDPs
-
-#     #         mdp_object = mdp.createMDP(
-#     #             self.states_num, self.actions_num, self.min_reward, self.max_reward
-#     #         )
-#     #         threshold = 0.001
-
-#     #         result = algorithm.learn_probabilities_convergence(mdp_object, threshold)
-
-#     #         # Check if the sum of all probabilities is close to 1
-#     #         for state, action_foll_states in result.items():
-#     #             for action, foll_states in action_foll_states.items():
-#     #                 state_prob_sum = sum(list(foll_states.values()))
-#     #                 self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
+            # Check if the sum of all probabilities is close to 1
+            for state, action_foll_states in result.items():
+                for action, foll_states in action_foll_states.items():
+                    state_prob_sum = sum(list(foll_states.values()))
+                    self.assertAlmostEqual(state_prob_sum, 1.0, delta=1e-6)
 
 
 if __name__ == "__main__":
