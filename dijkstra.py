1f0c7f6618a58b172201c68abc6be51d11ff3a74
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-04-18 09:00:29 +0200

    Function that returns the biggest probability and corresponding action of transitioning to a certain state implemented

--- dijkstra.py
+++ dijkstra.py
@@ -1,15 +1,55 @@
 import sys
 
+import mdp
+
 # Idee fuer spaeter:
 # number of states hits einbeziehen, um die Loesung zu entwickeln, die auch selten besuchte Zustaende berueksichtigt
 
-# Returns directly connected states to a certain state
-def get_neighbour_states(probabilities, current_state):
-    return
 
-# Returns value of transition between two states
-def get_transition_probability(state1, state2):
-    return
+# Returns an action with the biggest probability between two states
+# Note: After each action it possible to go over to each of the states (each state connected with each state),
+# but for some of them the probability is equal to 0 (no connection)
+def neighbour_biggest_prob_value(mdp_states, approximated_prob, curr_visited_state):
+
+    # We shouldn't consider "the loop" transition - going again to the same state from which we start
+    mdp_states.remove(curr_visited_state)
+
+    # neihbour_prob - stores the biggest probability to neighbour (2) and responsible for it action (1)
+    # state = { (1)action : (2)probability}
+    neighbour_prob = {state: {None: None} for state in mdp_states}
+
+    # Retreive actions that are possible for curr_visited_state
+    poss_actions = mdp.get_possible_actions(approximated_prob, curr_visited_state)
+
+    # For each of the possible neighbours find the most probably transition
+    for state in mdp_states:
+
+        # Set initial values for max_action and max_pro
+        max_action = poss_actions[0]
+        max_prob = 0
+
+        for action in poss_actions:
+
+            new_max_prob = approximated_prob[curr_visited_state][action][state]
+
+            if new_max_prob > max_prob:
+
+                max_action = action
+                max_prob = new_max_prob
+
+                neighbour_prob[state] = {max_action: max_prob}
+
+    # Check if any value in the dictionary is {None: None} - it is not possible (prob = 0.0) to reach a state through any of available actions
+    no_neighbours = [
+        state for state, value in neighbour_prob.items() if value == {None: None}
+    ]
+
+    # Remove the "no-neighbours" from the dictionary
+    for state in no_neighbours:
+        del neighbour_prob[state]
+
+    return neighbour_prob
+
 
 def dijkstra_alg(mdp_object, approximated_prob, start_state, end_state):
 
@@ -21,16 +61,16 @@ def dijkstra_alg(mdp_object, approximated_prob, start_state, end_state):
     previous_nodes = {}
 
     max_value = sys.maxsize
-    print(max_value)
 
     for node in unvisited_nodes:
         shortest_path[node] = max_value
 
     # Initialize the starting node with 0
     shortest_path[start_state] = 0
+    print(shortest_path)
 
     # Execute the algorithm until the end_node is visited
     while end_state in unvisited_nodes:
-
+        return
 
     return
