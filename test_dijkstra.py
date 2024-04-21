efdb05df5f48ff6f1eb9fe2477188e83635b070e
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-04-21 16:07:41 +0200

    test_neigbour_biggest_prob test adjusted to NeighbourProbabilityEntry data structure

--- test_dijkstra.py
+++ test_dijkstra.py
@@ -20,7 +20,9 @@ class TestDijkstra(unittest.TestCase):
         # 2) "s0" -> "s1" -> "s2" -> "s3" (prob: 0.9 * 0.9 * 0.9 = 0.729)
 
         self.prob["s0"]["a1"]["s3"] = 0.1
-        self.prob["s0"]["a1"]["s1"] = 0.9
+        self.prob["s0"]["a1"]["s0"] = 0.9
+        self.prob["s0"]["a0"]["s1"] = 0.9
+        self.prob["s0"]["a0"]["s0"] = 0.1
 
         self.prob["s1"]["a2"]["s2"] = 0.9
         self.prob["s1"]["a2"]["s1"] = 0.1
@@ -40,36 +42,50 @@ class TestDijkstra(unittest.TestCase):
     def test_neighbour_biggest_prob(self):
 
         # Start - neighbours of s0, all states unvisited
-        s0_neighbours = {"s1": {"a1": 0.9}, "s3": {"a1": 0.1}}
-
         unvisited_states = self.states
+
+        s0_neighbour_s1 = dijkstra.NeighbourProbabilityEntry("a0", 0.9)
+        s0_neighbour_s3 = dijkstra.NeighbourProbabilityEntry("a1", 0.1)
+
+        s0_neighbours = {"s1": s0_neighbour_s1, "s3": s0_neighbour_s3}
+
         s0_result = dijkstra.neighbour_biggest_prob(unvisited_states, self.prob, "s0")
 
-        self.assertDictEqual(s0_neighbours, s0_result)
+        for key, values in s0_neighbours.items():
+            self.assertEqual(values.action, s0_result[key].action)
+            self.assertEqual(values.probability, s0_result[key].probability)
 
         unvisited_states.remove("s0")
 
         # Go over to s1 and extract neighbours
-        s1_neighbours = {"s2": {"a2": 0.9}}
+        s1_neighbour_s2 = dijkstra.NeighbourProbabilityEntry("a2", 0.9)
+        s1_neighbours = {"s2": s1_neighbour_s2}
 
         s1_result = dijkstra.neighbour_biggest_prob(unvisited_states, self.prob, "s1")
 
-        self.assertDictEqual(s1_neighbours, s1_result)
+        for key, values in s1_neighbours.items():
+            self.assertEqual(values.action, s1_result[key].action)
+            self.assertEqual(values.probability, s1_result[key].probability)
 
         unvisited_states.remove("s1")
 
         # Go over to s2 and extract neighbours
-        s2_neighbours = {"s3": {"a1": 0.9}}
+        s2_neighbour_s3 = dijkstra.NeighbourProbabilityEntry("a1", 0.9)
+        s2_neighbours = {"s3": s2_neighbour_s3}
 
         s2_result = dijkstra.neighbour_biggest_prob(unvisited_states, self.prob, "s2")
-        self.assertDictEqual(s2_neighbours, s2_result)
+
+        for key, values in s2_neighbours.items():
+            self.assertEqual(values.action, s2_result[key].action)
+            self.assertEqual(values.probability, s2_result[key].probability)
 
         # Check if not reachable states are deleted
         s3_neighbours = {}
         s3_result = dijkstra.neighbour_biggest_prob(self.states, self.prob, "s3")
         self.assertDictEqual(s3_neighbours, s3_result)
 
-    def test_probabilities(self):
+    # Function for printing purposes
+    def test_print(self):
         # Print the content of self.prob
         mdp.print_mdp_details(self.prob)
 
