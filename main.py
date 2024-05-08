3a484aceb9cf1599c83d42118bdafc64e2ad3d55
Mateusz Cygan <mateuszcygan@protonmail.com>
2024-05-08 15:51:20 +0200

    two mdps with alternating approach

--- main.py
+++ main.py
@@ -12,27 +12,18 @@ mdp_obj1 = generator.read_saved_mdp("mdp_obj1.pkl")
 mdp_debug = generator.read_saved_mdp("mdp_debug.pkl")
 mdp_dijkstra = generator.read_saved_mdp("mdp_dijkstra.pkl")
 
-mdp.print_mdp_details(mdp_obj1.probabilities)
-approx = algorithm.my_algorithm(mdp_obj1, 25, 25, 50)
-mdp.print_mdp_details(approx)
-
-
-# mdp.print_mdp_sets(mdp_obj0)
-# dijkstra_0 = dijkstra.dijkstra_alg(mdp_obj0.states, mdp_obj0.probabilities, "s0", "s2")
-# print(dijkstra_0)
-
-# mdp.print_mdp_sets(mdp_obj1)
-# dijkstra_1 = dijkstra.dijkstra_alg(mdp_obj1.states, mdp_obj1.probabilities, "s0", "s5")
-# print(dijkstra_1)
-
 # mdp.print_mdp_sets(mdp_debug)
-# dijkstra_debug = dijkstra.dijkstra_alg(
-#     mdp_debug.states, mdp_debug.probabilities, "s0", "s1"
-# )
-# print(dijkstra_debug)
 
-# mdp.print_mdp_sets(mdp_dijkstra)
-# dijkstra_dijkstra = dijkstra.dijkstra_alg(
-#     mdp_dijkstra.states, mdp_dijkstra.probabilities, "s0", "s3"
-# )
-# print(dijkstra_dijkstra)
+algorithm.my_algo_alternating(mdp_debug, 4, 0.1, 5, 25, 25)
+mdp.print_mdp_details(mdp_debug.probabilities)
+
+
+# Test
+approx_prob_test, states_hits_test = algorithm.my_algo_alternating(
+    mdp_obj0,
+    4,
+    0.001,
+    8,
+    10,
+    25,
+)
